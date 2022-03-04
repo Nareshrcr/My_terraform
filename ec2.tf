b85c061b7f703f91c95cc5789f25edf20061e827
naresh kumar <nareshrcr6655@gmail.com>
2022-03-04 07:46:48 +0530

    file is updated

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
-resources "aws" "my_first_ec2" {
+resource "aws" "my_first_ec2" {
     ami = "ami-08b6f2a5c291246a0"
     instance_type = "t2.micro"
 
     tags = {
         name = "terra"
-      }
+    }
 
 } 
