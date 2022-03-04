7fde10a300bf6ee1cbe303c8e86f6bcc06173ea3
naresh kumar <nareshrcr6655@gmail.com>
2022-03-04 15:27:10 +0530

    newcommit

--- multipleinstance.tf
+++ multipleinstance.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "myinstance" {
     count = 3
-    ami = "ami-08b6f2a5c291246a0"
+    ami ="ami-08b6f2a5c291246a0"
     instance_type = "t2.micro"
-}
 
-tag = {
-    Name = "threedemoinstances"
+    tags = {
+        Name = "3demoinstances"
+    }
 }
