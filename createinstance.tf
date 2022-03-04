821165ed81dd829b5e57a99c4e5f3227b1bb5a9c
naresh kumar <nareshrcr6655@gmail.com>
2022-03-04 12:07:35 +0530

    createdinstance

--- createinstance.tf
+++ createinstance.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-    access_key = ""
-    secret_key = ""
-    region = "us-east-2"
-}
-
 resource "aws_instance" "myfirstinstance" {
     ami = "ami-08b6f2a5c291246a0"
     instance_type = "t2.micro"
